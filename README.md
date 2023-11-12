c098561769d88550d31121ffd607d1e03c41d21b
mkeras <63744162+mkeras@users.noreply.github.com>
2023-11-11 16:11:39 -0800

    Bug fixing and development

--- README.md
+++ README.md
@@ -1,4 +1,39 @@
 # chatGPTAgent
- A container that subscribes to a given topic, processes the incoming messages using a given chatGPT model and prompt and publishes the result to a given topic. The idea is to have these configured to be connected to a UNS with specific tasks.
+## Overview
+This container is designed to function as a part of a Universal Naming System (UNS), acting as both a consumer and producer of events/information. It subscribes to a specified MQTT topic, processes incoming messages using a specified ChatGPT model and prompt, and then publishes the results to another topic. The container can be used independently or orchestrated in a sequence where one instance's output becomes the input for the next, enabling complex step by step data transformations.
 
- This project is under development. Between program logic and whats available with the chatGPT api (function calling, chaining prompts together, etc), there are many features and functions to add to this image.
+## Development Status
+- Under active development: Subject to significant changes as features are added and refined.
+- Expanding capabilities: Future updates will include more complex functions like function calling and automating of prompt container chaining.
+
+## Configuration
+Environment variables are used for configuration:
+
+- DEBUG: Enable debugging (e.g., true or false). Publish logs/error to the logs/debug topic.
+- OPENAI_API_KEY: Your OpenAI API Key
+- GPT_MODEL: GPT model identifier (e.g., gpt-3.5-turbo); typos can cause crashes
+- GPT_SYSTEM_PROMPT: System prompt for the model
+- GPT_USER_PROMPT: Optional user prompt added before incoming messages
+- OUTPUT_JSON: Format of output (true/false; refer to OpenAI API documentation (https://platform.openai.com/docs/api-reference/chat/create#chat-create-response_format))
+- MQTT Variables:
+    - MQTT_SUBSCRIBE_TOPIC: Topic to subscribe to (e.g., testing/chatGPTAgent/events/INCOMING-TEST)
+    - MQTT_PUBLISH_TOPIC: Topic to publish results (e.g., testing/chatGPTAgent/events/OUTGOING-TEST)
+    - MQTT_HOST: MQTT Broker Address/IP
+    - MQTT_PORT: Port for MQTT (1883 or 8883 for SSL)
+    - MQTT_USE_TLS: Use SSL (true/false)
+    - MQTT_USERNAME: MQTT username
+    - MQTT_PASSWORD: MQTT password
+    - MQTT_CLIENT_ID: MQTT Client ID (auto-set if left empty)
+- AGENT_NAME: Name for the agent, used in config and metrics topics (defaults to MQTT_CLIENT_ID)
+
+## Topic Structure
+The MQTT_SUBSCRIBE_TOPIC is the topic which will be fed into chatGPT. The MQTT_PUBLISH_TOPIC is where the resulting content from chatGPT will be published to.
+For now, the topic structure that the container uses is:
+- gpt-transform-v1/<AGENT_NAME>/metrics/status --> The online/offline status of the container instance. Retained Message.
+- gpt-transform-v1/<AGENT_NAME>/config --> The config dict of the chatGPT settings. Retained Message.
+- gpt-transform-v1/<AGENT_NAME>/metrics/message-processed --> The metrics published on every message processed. Includes token usage and the content that was published to the MQTT_PUBLISH_TOPIC. Retained Message
+- gpt-transform-v1/<AGENT_NAME>/metrics/logs/debug --> Topic where messages are published when DEBUG env var is set to true.
+
+## Features
+- Process/Transform data from an MQTT topic and publish it to another MQTT topic
+- Change chatGPT configuration parameters via MQTT. Right now this topic is overwritten whenever by the environment variables whenever the container starts, but 
--- source/app/__init__.py
+++ source/app/__init__.py
@@ -8,10 +8,15 @@ import uuid
 
 openai.api_key = env.OPENAI_API_KEY
 
-SYSTEM_PROMPT = {'role': 'system', 'content': env.GPT_SYSTEM_PROMPT}
-
 def millis() -> int:
     return int(time.time() * 1000)
+
+env_config = {
+    'chat-gpt-model': env.GPT_MODEL,
+    'system-prompt': env.GPT_SYSTEM_PROMPT,
+    'user-prompt': env.GPT_USER_PROMPT if env.GPT_USER_PROMPT else None,
+    'output-json': env.OUTPUT_JSON
+}
 
 config = {
     'chat-gpt-model': env.GPT_MODEL,
@@ -27,10 +32,12 @@ root_topic = f'gpt-transform-v1/{env.AGENT_NAME}/'
 
 config_topic = root_topic + 'config'
 log_topic = root_topic + 'metrics/logs/debug'
+status_topic = root_topic + 'metrics/status'
 
 
-def publish_config(client: mqtt.Client):
-    client.publish(config_topic, json.dumps({'timestamp': millis(), 'config': config}), retain=True)
+def publish_config(client: mqtt.Client, initial: bool = False):
+    client.publish(config_topic, json.dumps({'timestamp': millis(), 'config': config, 'initial-publish': initial}), retain=True)
+
 
 def on_config_change(client, userdata, message):
     try:
@@ -39,6 +46,11 @@ def on_config_change(client, userdata, message):
         if DEBUG:
             client.publish(log_topic, json.dumps({'eventType': 'error', 'error': str(err), 'function': 'on_config_change(client, userdata, message)'}))
         return
+
+    if new_config.get('initial-publish'):
+        if DEBUG:
+            client.publish(log_topic, json.dumps({'eventType': 'log', 'message': 'Ignoring initial publish of config', 'function': 'on_config_change(client, userdata, message)'}))
+        return
     
     new_config = new_config.get('config')
     if not new_config:
@@ -46,18 +58,22 @@ def on_config_change(client, userdata, message):
             client.publish(log_topic, json.dumps({'eventType': 'error', 'error': 'New Config Published without "config" key', 'function': 'on_config_change(client, userdata, message)'}))
         return
     
+    #env_config = True
     config_changed = False
     try:
         for key, value in new_config.items():
+            # if key not in env_config.keys() or value != env_config[key]:
+            #     env_config = False
             if key not in config.keys() or value == config[key]:
                 # Ignore Keys that don't belong
                 continue
             config[key] = value
             config_changed = True
+    
 
     except Exception as err:
         if DEBUG:
-            client.publish(log_topic, json.dumps({'eventType': 'error', 'error': str(err), 'function': 'on_config_change(client, userdata, message)'}))
+            client.publish(log_topic, json.dumps({'eventType': 'error', 'config': config, 'error': str(err), 'function': 'on_config_change(client, userdata, message)'}))
         return
 
     if not config_changed:
@@ -65,7 +81,8 @@ def on_config_change(client, userdata, message):
             client.publish(log_topic, json.dumps({'eventType': 'log', 'message': 'new unchanged config received', 'function': 'on_config_change(client, userdata, message)'}))
         return
     
-    publish_config(client)
+    if DEBUG:
+        client.publish(log_topic, json.dumps({'eventType': 'log', 'message': 'Config changed successfully', 'function': 'on_config_change(client, userdata, message)'}))
 
 
 def on_incoming_data(client, userdata, message):
@@ -76,7 +93,7 @@ def on_incoming_data(client, userdata, message):
     message_uuid = str(uuid.uuid4())
     
     prompt = [
-        SYSTEM_PROMPT,
+        {'role': 'system', 'content': config['system-prompt']},
         {'role': 'user', 'content': f'{config["user-prompt"]}\n\n{message.payload.decode()}' if config['user-prompt'] else f'{message.payload.decode()}'}
     ]
 
@@ -106,8 +123,7 @@ def on_incoming_data(client, userdata, message):
 
 
 def on_connect(client, userdata, flags, rc):
-    client.publish(root_topic+'metrics/STATE', b'', retain=True)
-    client.publish(root_topic+'metrics/status', json.dumps({'state': 'ONLINE', 'ts': millis()}), retain=True)
+    client.publish(status_topic, json.dumps({'state': 'ONLINE', 'ts': millis()}), retain=True)
     if DEBUG:
         client.publish(log_topic, f'Client Connected: {env.AGENT_NAME}')
 
@@ -117,7 +133,7 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe(config_topic)
     client.subscribe(env.MQTT_SUBSCRIBE_TOPIC)
 
-    publish_config(client)
+    publish_config(client, initial=True)
     
 
 
@@ -129,7 +145,7 @@ def create_mqtt_client() -> mqtt.Client:
 
     client.username_pw_set(username=env.MQTT_USERNAME, password=env.MQTT_PASSWORD)
 
-    client.will_set(topic=root_topic+'metrics/STATE', payload=json.dumps({'state': 'OFFLINE', 'will-set-ts': millis()}), retain=True)
+    client.will_set(topic=status_topic, payload=json.dumps({'state': 'OFFLINE', 'will-set-ts': millis()}), retain=True)
 
     client.on_connect = on_connect
 
